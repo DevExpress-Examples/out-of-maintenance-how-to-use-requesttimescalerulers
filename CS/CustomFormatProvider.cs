f83af2a57fbbda40047bd3844a988d24c9f9bffc
Ivan Yakovlev (DevExpress) <yakovlev.ivan@corp.devexpress.com>
2020-01-31 14:11:55 +0300

    add example content

---
+++ CS/CustomFormatProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DevExpress.Mvvm;
+
+namespace DXGridSample {
+    public class CustomFormatProvider : IFormatProvider, ICustomFormatter {
+        public TimeSpan Zoom { get; }
+        public CustomFormatProvider(TimeSpan zoom) {
+            Zoom = zoom;
+        }
+        public object GetFormat(Type formatType) {
+            if (typeof(ICustomFormatter) == formatType)
+                return this;
+            else
+                return null;
+        }
+        public string Format(string format, object arg, IFormatProvider formatProvider) {
+            var range = arg as DateTimeRange?;
+            if (!range.HasValue)
+                return null;
+            if (Zoom.Hours > 2)
+                return range.Value.Start.ToString("MMM");
+            return string.Format("{0:MMMM yyyy} - {1:MMMM yyyy}", range.Value.Start, range.Value.End);
+        }
+    }
+}
---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Gantt;
+
+namespace DXGridSample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void RequestTimescaleRulers(object sender, RequestTimescaleRulersEventArgs e) {
+            e.TimescaleRulers.Clear();
+            e.TimescaleRulers.Add(new TimescaleRuler(TimescaleUnit.Month, formatProvider: new CustomFormatProvider(ganttView.Zoom)));
+        }
+    }
+}
