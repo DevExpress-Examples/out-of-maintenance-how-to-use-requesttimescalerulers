7ca8ccdc7ec2ac3e44a3328eeed3c23b7a71a2b5
DevExpressExampleBot <fakeEmail>
2020-02-06 10:58:45 +0300

    Source auto update [skip ci]

---
+++ VB/CustomFormatProvider.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports DevExpress.Mvvm
+
+Namespace DXGridSample
+	Public Class CustomFormatProvider
+		Implements IFormatProvider, ICustomFormatter
+
+		Public Shared Function GetFormatProvider(ByVal zoom As TimeSpan) As CustomFormatProvider
+			Return New CustomFormatProvider(If(zoom.Hours > 2, "{0:MMM}", "{0:MMMM yyyy} - {1:MMMM yyyy}"))
+		End Function
+
+		Public ReadOnly Property FormatString() As String
+
+'INSTANT VB NOTE: The variable formatString was renamed since Visual Basic does not handle local variables named the same as class members well:
+		Public Sub New(ByVal formatString_Conflict As String)
+			Me.FormatString = formatString_Conflict
+		End Sub
+		Public Function GetFormat(ByVal formatType As Type) As Object Implements IFormatProvider.GetFormat
+			If GetType(ICustomFormatter) Is formatType Then
+				Return Me
+			Else
+				Return Nothing
+			End If
+		End Function
+'INSTANT VB NOTE: The parameter format was renamed since Visual Basic will not allow parameters with the same name as their enclosing function or property:
+		Public Function Format(ByVal format_Conflict As String, ByVal arg As Object, ByVal formatProvider As IFormatProvider) As String Implements ICustomFormatter.Format
+			Dim range = CType(arg, DateTimeRange?)
+			Return If(range.HasValue, String.Format(FormatString, range.Value.Start, range.Value.End), Nothing)
+		End Function
+	End Class
+End Namespace
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Windows
+Imports DevExpress.Xpf.Gantt
+
+Namespace DXGridSample
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub RequestTimescaleRulers(ByVal sender As Object, ByVal e As RequestTimescaleRulersEventArgs)
+			e.TimescaleRulers.Clear()
+			e.TimescaleRulers.Add(New TimescaleRuler(TimescaleUnit.Month, formatProvider:= CustomFormatProvider.GetFormatProvider(ganttView.Zoom)))
+		End Sub
+	End Class
+End Namespace
